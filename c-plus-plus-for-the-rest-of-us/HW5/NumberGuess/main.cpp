34c10ac09ce02252018b90541ef0975bef9b1b69
Manish Singh <singhmanish.personal@gmail.com>
2019-03-11 12:21:59 +0530

    HW5

--- c-plus-plus-for-the-rest-of-us/HW5/NumberGuess/main.cpp
+++ c-plus-plus-for-the-rest-of-us/HW5/NumberGuess/main.cpp
@@ -5,22 +5,24 @@ using namespace std;
 int main()
 {
 
-    int highest=100, lowest=0, attempts=0, guess, pguess;
+    int highest=100, lowest=0, attempts=0, guess, pguess, invalidOption=0;
     char option;
     cout << "Think of a number between 1 and 100" << endl;
 
     while(option!='q')
     {
-
         attempts++;
         pguess=guess;
         guess= lowest + ((highest - lowest) * 0.5);
         cout<<"I guess "<<guess<<". Am I right?"<<endl<<"'q' to quit, 'y' if correct, 'h' if too high, 'l' if too low."<<endl;
         cin>>option;
 
-        if(pguess==guess){
+        if(pguess==guess && invalidOption==0)
+        {
             break;
         }
+
+        invalidOption=0;
 
         if(option=='y' || option=='Y')
         {
@@ -34,6 +36,11 @@ int main()
         else if(option=='l' || option=='L')
         {
             lowest=guess;
+        }
+        else
+        {
+            cout<<"I didn't understand that response."<<endl;
+            invalidOption=1;
         }
     }
 
