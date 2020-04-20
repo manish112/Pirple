630cf669759a0a900813979173637ac3c73f8003
Manish Singh <manising@adobe.com>
2020-04-20 12:12:41 +0530

    Week2

---
+++ python-is-easy/Week#1/Main.py
@@ -0,0 +1,19 @@
+"""
+This is a python code which has attributes 
+about a song
+"""
+
+
+Artist = "Dave Brubeck" #artist name
+Genre = "Jazz" #genre of the song
+DurationInSeconds = 328 #song duration in seconds
+year=2015 #year in which the song was released
+ratings=4.5 #ratings of this sonng
+songname="Hello" #song anme
+
+print(Artist)
+print(Genre)
+print(DurationInSeconds)
+print(year)
+print(ratings)
+print(songname)
---
+++ python-is-easy/Week#2/Main.py
@@ -0,0 +1,35 @@
+"""
+This is a python code which has attributes 
+about a song
+"""
+
+
+Artist = "Dave Brubeck" #artist name
+Genre = "Jazz" #genre of the song
+DurationInSeconds = 328 #song duration in seconds
+year=2015 #year in which the song was released
+ratings=4.5 #ratings of this sonng
+songname="Hello" #song anme
+inmarket=True
+
+
+def inmarket1():
+	return inmarket
+
+def artist():
+	return Artist
+
+def year1():
+	return year
+	
+def ratings1():
+	return ratings
+	
+
+print(artist())
+print(Genre)
+print(DurationInSeconds)
+print(year1())
+print(ratings1())
+print(songname)
+print(inmarket1())
